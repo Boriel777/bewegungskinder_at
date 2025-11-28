4ce3caf82fee328c7424b1aef0d351343c51521f
Dalibor Durbas <office@boriel-designs.com>
2025-11-28 13:43:43 +0100

    refract slider styles

--- borielblank_child/index.php
+++ borielblank_child/index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <article class="mb-4">
-            <h2><?php the_title(); ?></h2>
-            <div class="post-excerpt"><?php the_excerpt(); ?></div>
-        </article>
-    <?php endwhile; ?>
-<?php endif; ?>
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+<article class="mb-4">
+    <h2><?php the_title(); ?></h2>
+    <?php if (has_post_thumbnail()) the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
+    <div><?php the_excerpt(); ?></div>
+    <a class="btn btn-primary" href="<?php the_permalink(); ?>">Read more</a>
+</article>
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
--- borielblank_child/page.php
+++ borielblank_child/page.php
@@ -3,8 +3,9 @@
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 <article class="mb-4">
     <p class="lead"><?php the_title(); ?></p>
-    <div><?php the_content(); ?></div>
+    <?php the_content(); ?>
 </article>
 <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
+
