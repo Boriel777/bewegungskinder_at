15bb040264eb04f301e13541158efa9888a597a9
Dalibor Durbas <office@boriel-designs.com>
2026-01-17 17:36:41 +0100

    refract ux footer

--- borielblank_child/footer.php
+++ borielblank_child/footer.php
@@ -9,7 +9,7 @@ $footer_post = get_page_by_title('Footer', OBJECT, 'post');
 if ($footer_post) {
     echo apply_filters('the_content', $footer_post->post_content);
 } else {
-    echo '<p>Please create a post titled "Footer" to display here.</p>';
+    echo '<p>Please create a post titled "Footer" to display here. </br> Bitte erstelle einen Beitrag mit dem Titel „Footer“, der hier angezeigt wird.</p>';
 }
 ?>
 <a class="bd_tribute" href="https://www.boriel-designs.com" target="_blank" rel="noopener noreferrer"><p>Made by: <img src="http://bewegungskinder.local/wp-content/uploads/2025/10/White_Mark_Boriel_Designs@3x.png" alt="boriel designs logo"></p></a>
